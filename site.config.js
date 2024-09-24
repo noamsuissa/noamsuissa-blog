e285690f605c5fe19e9c9e628f391ce4aabf7d67
Noam Suissa <noam.suissa01@gmail.com>
2024-09-24 16:10:12 -0400

    added og png

--- site.config.js
+++ site.config.js
@@ -24,10 +24,10 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://blog.noamaidev.app",
+  link: "https://blog.noamaidev.com",
   since: 2024, // If leave this empty, current year will be used.
   lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: "https://blog.noamaidev.com/og1.png", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
   notionConfig: {
