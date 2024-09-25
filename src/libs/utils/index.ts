6c8a7165311d4d423fd797b24a4fbd804db309d9
Noam Suissa <noam.suissa01@gmail.com>
2024-09-25 14:05:05 -0400

    timezone fix

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -4,7 +4,7 @@ export function formatDate(date: any, local: any) {
     year: 'numeric',
     month: 'short',
     day: 'numeric',
-    timeZone: 'America/New_York' // Use 'America/New_York' for GMT-4 (Eastern Time)
+    timeZone: 'UTC'
   };
   const res = d.toLocaleDateString(local, options);
   return res;
