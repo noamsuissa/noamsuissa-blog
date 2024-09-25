c3bcc4c591fca0678ec799739cfe3d8c2bce0798
Noam Suissa <noam.suissa01@gmail.com>
2024-09-25 13:37:14 -0400

    Update index.ts

--- src/libs/utils/index.ts
+++ src/libs/utils/index.ts
@@ -1,6 +1,11 @@
 export function formatDate(date: any, local: any) {
-  const d = new Date(date)
-  const options: any = { year: 'numeric', month: 'short', day: 'numeric' }
-  const res = d.toLocaleDateString(local, options)
-  return res
+  const d = new Date(date);
+  const options: any = {
+    year: 'numeric',
+    month: 'short',
+    day: 'numeric',
+    timeZone: 'America/New_York' // Use 'America/New_York' for GMT-4 (Eastern Time)
+  };
+  const res = d.toLocaleDateString(local, options);
+  return res;
 }
