376dffc7ca8d15d4a833ae5f58efbf0ef507f625
Noam Suissa <noam.suissa01@gmail.com>
2024-09-26 15:58:56 -0400

    added vercel analystics

--- src/layouts/RootLayout/index.tsx
+++ src/layouts/RootLayout/index.tsx
@@ -39,6 +39,7 @@ import 'prismjs/components/prism-swift.js'
 import 'prismjs/components/prism-wasm.js'
 import 'prismjs/components/prism-yaml.js'
 import "prismjs/components/prism-go.js"
+import { Analytics } from "@vercel/analytics/react"
 
 type Props = {
   children: ReactNode
@@ -56,6 +57,7 @@ const RootLayout = ({ children }: Props) => {
       <Scripts />
       {/* // TODO: replace react query */}
       {/* {metaConfig.type !== "Paper" && <Header />} */}
+      <Analytics />
       <Header fullWidth={false} />
       <StyledMain>{children}</StyledMain>
     </ThemeProvider>
